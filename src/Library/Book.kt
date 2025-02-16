3cfb46805bd41c6fae72a016b854bc32437a6646
M1-49-mariam <mariam.almutairi19@gmail.com>
2025-02-16 20:12:36 +0300

    done task

--- src/Library/Book.kt
+++ src/Library/Book.kt
@@ -4,5 +4,9 @@ open class Book(val title: String, val author: String, val iSBN: Int) {
 
     fun displayInfo(){
         println("Title: $title, Author: $author, ISBN: $iSBN")
+
     }
+
+    override fun toString() = "title='$title', author='$author', ISBN=$iSBN"
+
 }
--- src/Main.kt
+++ src/Main.kt
@@ -6,10 +6,11 @@ fun main() {
         12323, "Horror")
     var book2 = NonFictionBook("non fiction", "author of book",
         123, "history")
-    book1.displayInfo()
+    println(book1.toString())
+ //   book1.displayInfo()
     book1.getGenre()
-
-    book2.displayInfo()
+    println(book2.toString())
+  //  book2.displayInfo()
     book2.getSubject()
 
 }
